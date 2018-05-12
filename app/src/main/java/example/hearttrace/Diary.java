20ef18bd1f98efb75e8714f42b2fb8a1e31e8af9
hzphzp <huangzp1104@gmail.com>
2018-05-12 08:53:21 +0800

    fixed the double id bug

--- app/src/main/java/example/hearttrace/Diary.java
+++ app/src/main/java/example/hearttrace/Diary.java
@@ -10,7 +10,7 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "Diary")
 public class Diary extends Record {
     public static final String TAG = "Diary";
-    @DatabaseField(id = true, generatedId = true, columnName = "TAG")
+    @DatabaseField(generatedId = true, columnName = "TAG")
     private int id;
     @DatabaseField(foreign = true)
     Diarybook diarybook;
