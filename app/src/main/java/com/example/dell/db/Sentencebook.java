b497c1b2dfaaef19c0d7149cdb4b1d5e27473c0f
shirley-wu <shirley0@mail.ustc.edu.cn>
2018-06-02 21:50:08 +0800

    fix

--- app/src/main/java/com/example/dell/db/Sentencebook.java
+++ app/src/main/java/com/example/dell/db/Sentencebook.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.stmt.DeleteBuilder;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
@@ -54,12 +56,55 @@ public class Sentencebook {
         }
     }
 
-    public void deteleSubSentence(DatabaseHelper helper) {
+    public void deleteSubSentence(DatabaseHelper helper) {
         try {
             Dao<Sentence, Integer> dao = helper.getSentenceDao();
-            // dao.deleteBuilder().where().eq("Sentencebook", sentencebookName).delete();
-            // TODO: Cannot compile
+            DeleteBuilder<Sentence, Integer> deleteBuilder = dao.deleteBuilder();
+
+            deleteBuilder.where().eq("Sentencebook", this);
+            deleteBuilder.delete();
         }catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't dao database", e);
+            throw new RuntimeException(e);
+        }
+    }
+    public void insert(DatabaseHelper helper) {
+        try {
+            Dao<Sentencebook, Integer> dao = helper.getSentencebookDao();
+            Log.i("sentencebook", "dao = " + dao + " 插入 sentencebook " + this);
+            int returnValue = dao.create(this);
+            Log.i("sentencebook", "插入后返回值：" + returnValue);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't dao database", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(DatabaseHelper helper) {
+        try {
+            Dao<Sentencebook, Integer> dao = helper.getSentencebookDao();
+            Log.i("sentencebook", "dao = " + dao + " 更新 sentencebook " + this);
+            int returnValue = dao.update(this);
+            Log.i("sentencebook", "更新后返回值：" + returnValue);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't dao database", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(DatabaseHelper helper) {
+        try {
+            Dao<Sentence, Integer> subdao = helper.getSentenceDao();
+            DeleteBuilder<Sentence, Integer> deleteBuilder = subdao.deleteBuilder();
+
+            deleteBuilder.where().eq("Sentencebook", this);
+            deleteBuilder.delete();
+
+            Dao<Sentencebook, Integer> dao = helper.getSentencebookDao();
+            Log.i("sentencebook", "dao = " + dao + " 删除 sentencebook " + this);
+            int returnValue = dao.delete(this);
+            Log.i("sentencebook", "删除后返回值：" + returnValue);
+        } catch (SQLException e) {
             Log.e(DatabaseHelper.class.getName(), "Can't dao database", e);
             throw new RuntimeException(e);
         }
