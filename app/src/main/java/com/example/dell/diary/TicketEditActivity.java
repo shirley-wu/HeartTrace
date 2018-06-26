ca30bc3b5dfc7e44358fa2e0881129a70d4b92bc
e <huyuchen@mail.ustc.edu.cn>
2018-06-26 22:38:23 +0800

    sentence: add label done

--- app/src/main/java/com/example/dell/diary/TicketEditActivity.java
+++ app/src/main/java/com/example/dell/diary/TicketEditActivity.java
@@ -1,17 +1,41 @@
 package com.example.dell.diary;
 
+import android.animation.ObjectAnimator;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dell.db.DatabaseHelper;
+import com.example.dell.db.Diary;
+import com.example.dell.db.Label;
 import com.example.dell.db.Sentence;
 import com.example.dell.db.Sentencebook;
 import com.example.dell.diary.R;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class TicketEditActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String BOTTLE_NAME = "bottle_name";
@@ -19,14 +43,32 @@ public class TicketEditActivity extends AppCompatActivity implements View.OnClic
     public static final String NOTE_EDITABLE = "note_editable";
     public static final String NOTE_NEW = "true";
     private static final String TAG = "TicketEditActivity";
-
+    private String sentence_name = "travel";
+    private String tag= null;
+    private List<ImageView> imageItems = new ArrayList<ImageView>(3);
+    private int[] imgIds = {R.drawable.travel,
+            R.drawable.study, R.drawable.work};
+    List<Sentence> sentenceList = new ArrayList<>();
+    List<Label> labelList = new ArrayList<>();
+    List<Label> label_this = new ArrayList<>();
+    private final int ICON_LIST_DIALOG = 1;
+    ActionBar actionBar;
+    private TextView sentenceDate;
+    private TextView sentenceWeekday;
+    private ImageView sentenceIcon;
+    private ImageView sentenceIcon1;
+    private ImageView sentenceIcon2;
+    private ImageView sentenceIcon3;
+    private ImageView sentenceIcon4;
 
     private FloatingActionButton ticket_confirm;
     private FloatingActionButton ticket_edit;
     private EditText editText ;
     private String note_editable = "true";
     private Sentence sentence;
-    private String note_new;
+    private ObjectAnimator objAnimatorX;
+    private int flag = 1;
+    private String note_new;public List<String> weekList = new ArrayList<>(Arrays.asList("周日","周一","周二","周三"," 周四","周五","周六"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,29 +84,61 @@ public class TicketEditActivity extends AppCompatActivity implements View.OnClic
         ticket_edit = (FloatingActionButton) findViewById(R.id.ticket_edit);
         ticket_confirm.setOnClickListener(this);
         ticket_edit.setOnClickListener(this);
+
+        sentenceIcon = (ImageView) findViewById(R.id.sentence_content_icon);
+        sentenceIcon1 = (ImageView) findViewById(R.id.sentence_content_icon1);
+        sentenceIcon2 = (ImageView) findViewById(R.id.sentence_content_icon2);
+        sentenceIcon3 = (ImageView) findViewById(R.id.sentence_content_icon3);
+        sentenceIcon4 = (ImageView) findViewById(R.id.sentence_content_icon4);
+        sentenceIcon.setOnClickListener(this);
+
         Init();
     }
 
     void Init(){
+        DatabaseHelper helper = new DatabaseHelper(getApplicationContext());
+        imageItems.add(sentenceIcon1);
+        imageItems.add(sentenceIcon2);
+        imageItems.add(sentenceIcon3);
+        imageItems.add(sentenceIcon4);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_sentence_content);
+        sentenceDate = (TextView)findViewById(R.id.sentence_date);
+        sentenceWeekday = (TextView)findViewById(R.id.sentence_weekday);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         if(note_editable.equals("false") ){
+            editText.setText(sentence.getText());
+            getLabelsOfSentence(sentence,helper);
+            String date = (sentence.getDate().getYear()+1900)+"年"+(sentence.getDate().getMonth()+1)+"月"+sentence.getDate().getDate()+"日";
+            sentenceDate.setText(date);
+            sentenceWeekday.setText(weekList.get(sentence.getDate().getDay()));
+
             ticket_confirm.setVisibility(View.INVISIBLE);
             editText.setEnabled(false);
-            editText.setText(sentence.getText());
-
+            actionBar.show();
         }
         else if (note_editable.equals("true")){
+            Date date = new Date();
+            String today = (date.getYear()+1900)+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日";
+            sentenceDate.setText(today);
+            sentenceWeekday.setText(weekList.get(date.getDay()));
+
             ticket_edit.setVisibility(View.INVISIBLE);
             editText.setEnabled(true);
             editText.setText(sentence.getText());
+            actionBar.hide();
         }
     }
-
-
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.ticket_confirm:
+                actionBar.show();
                 ticket_edit.setVisibility(View.VISIBLE);
                 ticket_confirm.setVisibility(View.INVISIBLE);
                 editText.setEnabled(false);
@@ -86,10 +160,172 @@ public class TicketEditActivity extends AppCompatActivity implements View.OnClic
                 }
                 break;
             case R.id.ticket_edit:
+                actionBar.hide();
                 ticket_edit.setVisibility(View.INVISIBLE);
                 ticket_confirm.setVisibility(View.VISIBLE);
                 editText.setEnabled(true);
                 break;
+            case R.id.sentence_content_icon:
+                for(int i=0;i<imageItems.size();i++) {
+                    int radius = 90;
+                    float distanceX = (float) (flag * radius * (i + 1));
+                    objAnimatorX = ObjectAnimator.ofFloat(imageItems.get(i), "x", imageItems.get(i).getX(), imageItems.get(i).getX() + distanceX);
+                    objAnimatorX.setDuration(120);
+                    objAnimatorX.setStartDelay(50);
+                    objAnimatorX.start();
+                    if(flag == 1) imageItems.get(i).setVisibility(View.VISIBLE);
+                    else if(flag == -1) imageItems.get(i).setVisibility(View.INVISIBLE);
+                }
+                flag = -flag;
+                break;
+
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sentence_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.label_sentence:
+                showDialog(ICON_LIST_DIALOG);
+                break;
+
+            case android.R.id.home:
+                finish();
+                return true;
+
+            default:
+        }
+        return true;
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        switch(id) {
+            case ICON_LIST_DIALOG:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setIcon(R.drawable.mood);
+                builder.setTitle("贴个标签？");
+                BaseAdapter adapter = new TicketEditActivity.ListItemAdapter();
+                DialogInterface.OnClickListener listener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                if(which == 0) sentence_name = "travel";
+                                else if(which == 1) sentence_name = "study";
+                                else if(which == 2) sentence_name = "work";
+                                else sentence_name = "travel";
+
+                                DatabaseHelper helper = new DatabaseHelper(getApplicationContext());
+
+                                Label label = Label.getByName(helper,sentence_name);
+                                if(label == null) {
+                                    label = new Label(sentence_name);
+                                    label.insert(helper);
+                                }
+                                sentence.insertLabel(helper, label);
+                                getLabelsOfSentence(sentence,helper);
+
+                            }
+                        };
+                builder.setAdapter(adapter, listener);
+                dialog = builder.create();
+                break;
+        }
+        return dialog;
+    }
+
+    class ListItemAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return imgIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position,
+                            View contentView, ViewGroup parent) {
+            TextView textView =
+                    new TextView(TicketEditActivity.this);
+            //获得array.xml中的数组资源getStringArray返回的是一个String数组
+            String text = getResources().getStringArray(R.array.mood)[position];
+            textView.setText(text);
+            //设置字体大小
+            textView.setTextSize(20);
+            AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(
+                    WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+            textView.setLayoutParams(layoutParams);
+            //设置水平方向上居中
+            textView.setGravity(android.view.Gravity.CENTER_VERTICAL);
+            textView.setMinHeight(65);
+            //设置文字颜色
+            textView.setTextColor(Color.BLACK);
+            //设置图标在文字的左边
+            textView.setCompoundDrawablesWithIntrinsicBounds(imgIds[position], 0, 0, 0);
+            //设置textView的左上右下的padding大小
+            textView.setPadding(30, 10, 30, 10);
+            //设置文字和图标之间的padding大小
+            textView.setCompoundDrawablePadding(25);
+            return textView;
+        }
+    }
+
+    public Drawable setTags(String id){
+        switch(id)
+        {
+            case "travel":
+                return (getResources().getDrawable(R.drawable.travel));
+            case "study":
+                return (getResources().getDrawable(R.drawable.study));
+            case "work":
+                return (getResources().getDrawable(R.drawable.work));
+            default:
+                return (getResources().getDrawable(R.drawable.travel));
+        }
+    }
+
+    public void getLabelsOfSentence(Sentence sentence, DatabaseHelper helper ){
+        sentenceIcon.setImageDrawable(getResources().getDrawable(R.drawable.nothing));
+        for(int i = 0; i<=3; i++){
+            imageItems.get(i).setImageDrawable(getResources().getDrawable(R.drawable.nothing));
+        }
+        label_this = null;
+        try {
+            label_this = sentence.getAllLabel(helper);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if(label_this  == null || label_this.size() == 0 || label_this.get(0).getLabelname() == null){
+            tag = "travel";
+        }
+        else{
+            int length = label_this.size();
+            tag = label_this.get(length - 1).getLabelname();
+            sentenceIcon.setImageDrawable(setTags(tag));
+            if(length >= 2 && length <=5)
+                for(int i = 0; i<= length-2; i++){
+                    tag = label_this.get(i).getLabelname();
+                    imageItems.get(length - 2 - i).setImageDrawable(setTags(tag));
+                }
+            else if(length >=6)
+                for(int i = 0; i<=3; i++){
+                    tag = label_this.get(length - 2 - i).getLabelname();
+                    imageItems.get(i).setImageDrawable(setTags(tag));
+                }
         }
     }
 }
