8164e8e4b43d18763c1ee05e4680306976cff9c9
shirley-wu <shirley0@mail.ustc.edu.cn>
2018-07-15 12:11:40 +0800

    my account

--- app/src/main/java/com/example/dell/auth/MyAccount.java
+++ app/src/main/java/com/example/dell/auth/MyAccount.java
@@ -21,32 +21,110 @@ public class MyAccount {
 
     private String token;
 
+    private String nickname;
+
+    private String gender;
+
+    private String birthday;
+
+    private String email;
+
+    private String school;
+
+    private String signature;
+
+    private int image;
+
     private MyAccount() {
 
     }
 
-    public boolean setName(String name) {
+    public void setName(String name) {
         this.name = name;
-
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("name", name);
-        return editor.commit();
     }
 
     public String getName() {
         return name;
     }
 
-    public boolean setToken(String token) {
+    public void setToken(String token) {
         this.token = token;
-
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("token", token);
-        return editor.commit();
     }
 
     public String getToken() {
         return token;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthday() {
+        return  birthday;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public boolean save() {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("name", name);
+        editor.putString("token", token);
+        editor.putString("nickname", nickname);
+        editor.putString("gender", gender);
+        editor.putString("birthday", birthday);
+        editor.putString("email", email);
+        editor.putString("school", school);
+        editor.putString("signature", signature);
+        editor.putInt("image", image);
+
+        return editor.commit();
     }
 
     static public MyAccount get(Context context) {
@@ -54,8 +132,15 @@ public class MyAccount {
             preferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
 
             myAccount   = new MyAccount();
-            myAccount.name  = preferences.getString("", null);
+            myAccount.name = preferences.getString("name", null);
             myAccount.token = preferences.getString("token", null);
+            myAccount.nickname = preferences.getString("nickname", null);
+            myAccount.gender = preferences.getString("gender", null);
+            myAccount.birthday = preferences.getString("birthday",null);
+            myAccount.email = preferences.getString("email", null);
+            myAccount.school = preferences.getString("school", null);
+            myAccount.signature = preferences.getString("signature", null);
+            myAccount.image = preferences.getInt("image", -1);
         }
         return myAccount;
     }
