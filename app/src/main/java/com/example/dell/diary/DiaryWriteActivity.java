67eb328b63256a7ba41c5b932281488632c17687
shirley-wu <shirley0@mail.ustc.edu.cn>
2018-08-06 18:23:27 +0800

    image getter into a seperate class

--- app/src/main/java/com/example/dell/diary/DiaryWriteActivity.java
+++ app/src/main/java/com/example/dell/diary/DiaryWriteActivity.java
@@ -424,19 +424,8 @@ public class DiaryWriteActivity extends AppCompatActivity implements View.OnClic
             } else {
                 Log.i("show", diary.getHtmlText());
                 if (diary.getHtmlText() != null) {
-                    diary_write.setText(Html.fromHtml(
-                            diary.getHtmlText(),
-                            new Html.ImageGetter() {
-                                @Override
-                                public Drawable getDrawable(String s) {
-                                    Picture picture = new Picture(s);
-                                    Bitmap bitmap = picture.getBitmap(DiaryWriteActivity.this);
-                                    bitmap = scaledImageForScreen(bitmap);
-                                    return new BitmapDrawable(bitmap);
-                                }
-                            },
-                            null
-                    ));
+                    diary_write.setText(Html.fromHtml(diary.getHtmlText()));
+                    getImage(diary.getText());
                     //clear color span
                     ForegroundColorSpan[] colorSpans = diary_write.getText().getSpans(0, diary_write.length(), ForegroundColorSpan.class);
                     for (int i = 0; i < colorSpans.length; i++)
@@ -1740,39 +1729,6 @@ public class DiaryWriteActivity extends AppCompatActivity implements View.OnClic
         isInsertingImg = false;
     }
 
-    public Bitmap scaledImageForScreen(Bitmap bitmap) {
-        // 获取屏幕参数
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        float widthPixels = displayMetrics.widthPixels * 0.8F;
-        Log.d(TAG, "scaledImage: widthPixels = " + widthPixels);
-
-        float density = displayMetrics.density;
-        Log.d(TAG, "scaledImage: density = " + density);
-
-        return scaledImage(bitmap, widthPixels * density);
-    }
-
-    public Bitmap scaledImage(Bitmap image, float targetWidth) {
-        // 获取图片宽高
-        int width = image.getWidth();
-        Log.d(TAG, "scaledImage: width = " + width);
-        int height = image.getHeight();
-
-        // 创建操作图片用的matrix对象
-        Matrix matrix = new Matrix();
-
-        // 计算宽高缩放率
-        float scale = targetWidth / width;
-        Log.d(TAG, "scaledImage: scale = " + scale);
-
-        // 缩放图片
-        matrix.postScale(scale, scale);
-        Bitmap new_image = Bitmap.createBitmap(image, 0, 0, width, height, matrix, true);
-        Log.d(TAG, "scaledImage: new_image width = " + new_image.getWidth());
-        return new_image;
-    }
-
     public String saveBitmap(Bitmap mBitmap) {
         try {
             Picture picture = new Picture();
@@ -2823,3 +2779,55 @@ class PicUtils {
 
 }
 
+
+class MyImageGetter implements Html.ImageGetter {
+
+    private final static String TAG = "MyImageGetter";
+
+    private DiaryWriteActivity mActivity = null;
+
+    public MyImageGetter(DiaryWriteActivity activity) {
+        mActivity = activity;
+    }
+
+    @Override
+    public Drawable getDrawable(String s) {
+        Picture picture = new Picture(s);
+        Bitmap bitmap = picture.getBitmap(mActivity);
+        bitmap = scaledImageForScreen(bitmap);
+        return new BitmapDrawable(bitmap);
+    }
+
+    private Bitmap scaledImageForScreen(Bitmap bitmap) {
+        // 获取屏幕参数
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        mActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        float widthPixels = displayMetrics.widthPixels * 0.8F;
+        Log.d(TAG, "scaledImageForScreen: widthPixels = " + widthPixels);
+
+        float density = displayMetrics.density;
+        Log.d(TAG, "scaledImageForScreen: density = " + density);
+
+        return scaledImage(bitmap, widthPixels * density);
+    }
+
+    private Bitmap scaledImage(Bitmap image, float targetWidth) {
+        // 获取图片宽高
+        int width = image.getWidth();
+        Log.d(TAG, "scaledImage: width = " + width);
+        int height = image.getHeight();
+
+        // 创建操作图片用的matrix对象
+        Matrix matrix = new Matrix();
+
+        // 计算宽高缩放率
+        float scale = targetWidth / width;
+        Log.d(TAG, "scaledImage: scale = " + scale);
+
+        // 缩放图片
+        matrix.postScale(scale, scale);
+        Bitmap new_image = Bitmap.createBitmap(image, 0, 0, width, height, matrix, true);
+        Log.d(TAG, "scaledImage: new_image width = " + new_image.getWidth());
+        return new_image;
+    }
+}
